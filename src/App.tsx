98bd226b83c6d29dcae4a70869c5438406286fb4
Alexander Senichev <a.senichev@tsconsulting.com>
2021-02-14 00:44:57 +0300

    base sidebar made, assets svgs added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
-import Dashboard from './Sidebar/Sidebar';
+import Sidebar from './Components/Sidebar/Sidebar';
 import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Dashboard/>
+        <Sidebar/>
       </div>
     </BrowserRouter>
   );
--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -2,36 +2,43 @@ import React from "react";
 import {Nav} from "react-bootstrap";
 import { withRouter } from 'react-router-dom';
 import './styles.scss';
+import { ReactComponent as GiftLogo }from '../../assets/gift.svg'
+import { ReactComponent as DeskLogo }from '../../assets/folder.svg'
+import { ReactComponent as EventLogo }from '../../assets/calendar.svg'
+import { ReactComponent as BlogLogo }from '../../assets/pen.svg'
 
 const Side = () => {
    
-    return (
-        <>
-    
-            <Nav className="col-md-12 d-none d-md-block bg-light sidebar"
-            activeKey="/home"
-            onSelect={selectedKey => console.log(`selected ${selectedKey}`)}
-            >
-                <div className="sidebar-sticky"></div>
-            <Nav.Item>
-                <Nav.Link href="/home">Active</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="link-1">Link</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="link-2">Link</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="disabled" disabled>
-                Disabled
-                </Nav.Link>
-            </Nav.Item>
-            </Nav>
-          
-        </>
-        );
-  };
+  return (
+    <>
+      <Nav className="flex-column sidebar style-wrapper"
+      variant="tabs"
+      activeKey="/home"
+      onSelect={selectedKey => console.log(`selected ${selectedKey}`)}
+      >
+        <Nav.Item>
+          <Nav.Link href="/home">
+            <GiftLogo className="logo"/>Желания
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="/wishes">
+            <DeskLogo className="logo"/>Доски
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="/events">
+            <EventLogo className="logo"/>События
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="/blog">
+            <BlogLogo className="logo"/>Блог
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </>
+  );};
 
   const Sidebar = withRouter(Side);
   export default Sidebar;
